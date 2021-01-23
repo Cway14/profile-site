dd7f89c759dae8bd68f3777398eebd917045b417
Cam Way <cam@Cams-MacBook-Pro.local>
2021-01-23 12:10:07 -0800

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  siteMetadata: {
+    title: "camway.ca",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "G-XYWS8G4N8X",
+      },
+    },
+  ],
+};
